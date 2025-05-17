7dc69281d2ea34ff5faf134aa944ac40828d4191
Douglas Felipe Mariano <159494725+Douglas-Felipe-Mariano@users.noreply.github.com>
2025-05-16 22:45:26 -0300

    Terminando a lista de tarefas

--- AtividadeMenuDeTarefa.java
+++ AtividadeMenuDeTarefa.java
@@ -6,13 +6,13 @@ public class AtividadeMenuDeTarefa {
         public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int opcao;
-        // String[] tarefas = new String[10];
         String novaTarefa;
+        int remover;
         ArrayList<String> tarefas = new ArrayList<>();
         
 
     do {
-
+        // menu de navegação
         System.out.println("Menu de Cadastro");
         System.out.println("1- Adicionar Tarefa");
         System.out.println("2- Listar Tarefas");
@@ -20,24 +20,40 @@ public class AtividadeMenuDeTarefa {
         System.out.println("4- Sair");
         System.out.println("Escolha uma opção");
         opcao = scanner.nextInt();
+        scanner.nextLine();
         
 
         switch (opcao) {
             case 1:
+                // adiciona tarefa
                 System.out.println("Informe o Nome da Tarefa:");
                 novaTarefa = scanner.nextLine();
                 tarefas.add(novaTarefa);
+                System.out.print("\n");
 
                 break;
             case 2:
+                // lista as tarefas atuais 
                 System.out.println("Tarefas Atuais:");
                 for (String tarefa : tarefas){
-                    System.out.println(tarefas);
+                    System.out.println(tarefa + "\n");
                 }
                 break;
             case 3:
                 System.out.println("Tarefas Atuais:");
-                System.out.println("Qual deseja Excluir?:");
+                // i = variavel de indice
+                int i=1;
+                // lista as tarefas
+                for (String tarefa : tarefas){
+                    System.out.println("[" + i + "]" + "-" + tarefa + "\n");
+                    i++;
+                }             
+                System.out.println("Qual Tarefa Deseja Excluir:");
+                // remove as a tarefa escolhida conforme o indice
+                remover = scanner.nextInt();
+                tarefas.remove(remover-1);
+                System.out.println("Tarefa Removida Com Sucesso \n");
+                break;
             case 4:
                 System.out.println("Saindo...");
                 break;
---
+++ CursoEmVideo/Exercicio.java
@@ -0,0 +1,10 @@
+package CursoEmVideo;
+import javax.swing.*;
+
+public class Exercicio {
+    
+    public static void main (String[] args){
+        System.out.println("Olá Mundo");
+    }
+
+}
